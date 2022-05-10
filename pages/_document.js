aae5f437605c21e76fb019592e61406b83791d4c
glenn <glennstreetman@gmail.com>
2022-05-10 16:56:25 +0000

    move public

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Righteous&family=Roboto:wght@100&family=Saira+Stencil+One&display=swap" rel="stylesheet" />
-                <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+                <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
             </Head>
             <body>
                 <Main />
