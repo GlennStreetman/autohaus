0cdf44b985801e05599c9616163e75cb07927e66
glenn <glennstreetman@gmail.com>
2022-06-01 19:12:00 +0000

    fix blank string bug

--- lib/parseMarkdown.tsx
+++ lib/parseMarkdown.tsx
@@ -8,18 +8,20 @@ interface props {
 
 function parseMarkdown(p: props) {
     const text = p.text
-        .replace(/^### (.*$)/gim, "<h3>$1</h3>") //three hashes
-        .replace(/^## (.*$)/gim, "<h2>$1</h2>") //two hashes
-        .replace(/^# (.*$)/gim, "<h1>$1</h1>") //1 hash
-        .replace(/^###--- (.*$)/gim, "<h3>&#x2022 $1</h3>") //three hashes Bullet point
-        .replace(/^##--- (.*$)/gim, "<h2>&#x2022 $1</h2>") //two hashes Bullet point
-        .replace(/^#--- (.*$)/gim, "<h1>&#x2022 $1</h1>") //1 hash Bullet point
-        .replace(/^--- (.*$)/gim, "&#x2022 $1") //--- sets bullet point at start of line
-        .replace(/\*Q(.*)\*Q/gim, "<blockquote>$1</blockquote>") // between *Q
-        .replace(/\*B(.*)\*B/gim, "<b>$1</b>") // between *B
-        .replace(/\*I(.*)\*I/gim, "<i>$1</i>") //between  *I
-        .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />") // ![text](link)
-        .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>"); // [text](link)
+        ? p.text
+              .replace(/^### (.*$)/gim, "<h3>$1</h3>") //three hashes
+              .replace(/^## (.*$)/gim, "<h2>$1</h2>") //two hashes
+              .replace(/^# (.*$)/gim, "<h1>$1</h1>") //1 hash
+              .replace(/^###--- (.*$)/gim, "<h3>&#x2022 $1</h3>") //three hashes Bullet point
+              .replace(/^##--- (.*$)/gim, "<h2>&#x2022 $1</h2>") //two hashes Bullet point
+              .replace(/^#--- (.*$)/gim, "<h1>&#x2022 $1</h1>") //1 hash Bullet point
+              .replace(/^--- (.*$)/gim, "&#x2022 $1") //--- sets bullet point at start of line
+              .replace(/\*Q(.*)\*Q/gim, "<blockquote>$1</blockquote>") // between *Q
+              .replace(/\*B(.*)\*B/gim, "<b>$1</b>") // between *B
+              .replace(/\*I(.*)\*I/gim, "<i>$1</i>") //between  *I
+              .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />") // ![text](link)
+              .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>") // [text](link)
+        : p.text;
     // .replace(/\n$/gim, "<br />"); // \n is newline
 
     const sanitizedData = () => ({
