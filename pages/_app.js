da0bba02d6d2b6e17b4c29a6fee056ec3e9ae900
Ubuntu <ubuntu@ip-172-31-37-199.us-east-2.compute.internal>
2022-06-08 16:43:55 +0000

    add meta description

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,15 @@ import React from "react";
 import ScreenWidth from "../components/screenWidth";
 
 function MyApp({ Component, pageProps }) {
+
     return (
         <>
             <div className="min-h-screen  relative">
                 <Head>
-                    <meta keywords="Porsche, Porche, Auto Repair, Oil Change, Brake Repair, Emissions Testing, Emissions Check, Check Engine Light, Car Inspection" />
+                    <meta 
+                        keywords="Porsche, Porche, Auto Repair, Oil Change, Brake Repair, Emissions Testing, Emissions Check, Check Engine Light, Car Inspection" 
+                        description={`${process.env.NEXT_PUBLIC_BUSINESS_NAME} Automotive Repair`} 
+                    />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </Head>
                 <ScreenWidth>
