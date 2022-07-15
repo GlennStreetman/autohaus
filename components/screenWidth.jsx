9cd8c4557226c3046cce8a7e92015f39efec7742
Ubuntu <ubuntu@ip-172-31-37-199.us-east-2.compute.internal>
2022-07-15 04:59:28 +0000

    update event listener to resize event

--- components/screenWidth.jsx
+++ components/screenWidth.jsx
@@ -1,10 +1,50 @@
+// import React, { useState, useEffect } from "react";
+
+// function getWindowDimensions() {
+//     const { width: width, height: height } = window?.visualViewport ? window?.visualViewport : {widtj: 0, height: 0};
+//     return {
+//         width,
+//         height,
+//     };
+// }
+
+// export const ScreenWidth = React.createContext();
+
+// function ScreenWidthComponent({ children }) {
+//     const [width, setWidth] = useState(0);
+//     useEffect(() => {
+//         setWidth(getWindowDimensions());
+//     }, []);
+
+//     useEffect(() => {
+//         //handle resize
+//         function handleResize() {
+//             setWidth(getWindowDimensions());
+//         }
+//         console.log('vw', Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0), 'vh', Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0))
+
+//         setTimeout(() => setWidth(getWindowDimensions(), 2000));
+//         setTimeout(() => setWidth(getWindowDimensions(), 10000));
+//         setTimeout(() => setWidth(getWindowDimensions(), 200000));
+
+//         window.visualViewport.addEventListener("resize", handleResize);
+//         return () => window.removeEventListener("resize", handleResize);
+//     }, []);
+
+//     return <ScreenWidth.Provider value={width}>{children}</ScreenWidth.Provider>;
+// }
+
+// export default ScreenWidthComponent;
+
+
 import React, { useState, useEffect } from "react";
 
 function getWindowDimensions() {
-    const { width: width, width: height } = window.visualViewport;
+    const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
     return {
         width,
-        // height,
+        height,
     };
 }
 
@@ -12,7 +52,6 @@ export const ScreenWidth = React.createContext();
 
 function ScreenWidthComponent({ children }) {
     const [width, setWidth] = useState(0);
-
     useEffect(() => {
         setWidth(getWindowDimensions());
     }, []);
@@ -22,12 +61,13 @@ function ScreenWidthComponent({ children }) {
         function handleResize() {
             setWidth(getWindowDimensions());
         }
+        console.log('vw', Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0), 'vh', Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0))
 
         setTimeout(() => setWidth(getWindowDimensions(), 2000));
         setTimeout(() => setWidth(getWindowDimensions(), 10000));
         setTimeout(() => setWidth(getWindowDimensions(), 200000));
 
-        window.visualViewport.addEventListener("resize", handleResize);
+        window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
