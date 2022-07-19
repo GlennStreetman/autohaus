c15d8b7f5862820a88dd77e12cc670ab666a505d
glenn <glennstreetman@gmail.com>
2022-07-19 18:38:23 +0000

    check undefined on p.faqw

--- components/manager/faq/mapFAQ.tsx
+++ components/manager/faq/mapFAQ.tsx
@@ -3,7 +3,7 @@ import LabeledSelect from "../../labeledSelect";
 import EditFAQ from "./editFAQ";
 import OutlinedSurface from "../../outlinedSurface";
 import { useState } from "react";
-import {faqObj} from '../../../pages/api/getFAQ'
+import { faqObj } from "../../../pages/api/getFAQ";
 
 interface props {
     faq: faqObj[];
@@ -42,75 +42,79 @@ function MapFAQ(p: props) {
     }
 
     // @ts-ignore
-    const mapResultLimit = Object.keys(Array.from({ length: Object.keys(p.faq).length })).map((key) => {
-        return (
-            <option key={`${key}-limit`} value={parseInt(key) + 1}>
-                {parseInt(key) + 1}
-            </option>
-        );
-    });
+    const mapResultLimit = p?.faq
+        ? Object.keys(Array.from({ length: Object.keys(p.faq).length })).map((key) => {
+              return (
+                  <option key={`${key}-limit`} value={parseInt(key) + 1}>
+                      {parseInt(key) + 1}
+                  </option>
+              );
+          })
+        : [];
 
-    const mapFAQ = p.faq.map((el) => {
-        const display = el.id.toString() === showDetail ? "" : "hidden";
-        const clickDetail = () => {
-            el.id.toString() === showDetail ? setShowDetail("-1") : setShowDetail(el.id.toString());
-        };
-        return (
-            <React.Fragment key={`${el.id}-FAQ-empty`}>
-                <tr key={`${el.id}-FAQ-edit`}>
-                    <td className="text-center">
-                        {" "}
-                        <input
-                            className=""
-                            type="checkbox"
-                            checked={edit !== false && el.id === edit.id}
-                            onChange={async () => {
-                                setEdit(el);
-                                p.setShowAdd();
-                            }}
-                        />
-                    </td>
-                    <td>
-                        <LabeledSelect
-                            label=""
-                            value={`${el.ordernumber}`}
-                            onClickCallback={(e) => {
-                                console.log("LIMIT", e);
-                                updateOrder(parseInt(e), el.id);
-                            }}
-                            id="newDay_ID"
-                        >
-                            {mapResultLimit}
-                        </LabeledSelect>
-                    </td>
-                    {/* <td onClick={clickDetail}>{el.name}</td> */}
-                    <td onClick={clickDetail}>{el.question}</td>
-                    {/* <td>{el.filename}</td> */}
-                    <td className="text-center">
-                        <input
-                            className=""
-                            type="checkbox"
-                            checked={false}
-                            onChange={async (e) => {
-                                e.preventDefault();
-                                // console.log('click', el.id)
-                                fetch(`/api/faq/deleteFAQ?id=${el.id}`)
-                                    .then((response) => response.json())
-                                    .then((data) => {
-                                        p.getFAQ();
-                                    });  
-                            }}
-                        />
-                    </td>
-                </tr>
-                {/* <tr key={`${el.id}-FAQ-delete`}>
+    const mapFAQ = p.faq
+        ? p.faq.map((el) => {
+              const display = el.id.toString() === showDetail ? "" : "hidden";
+              const clickDetail = () => {
+                  el.id.toString() === showDetail ? setShowDetail("-1") : setShowDetail(el.id.toString());
+              };
+              return (
+                  <React.Fragment key={`${el.id}-FAQ-empty`}>
+                      <tr key={`${el.id}-FAQ-edit`}>
+                          <td className="text-center">
+                              {" "}
+                              <input
+                                  className=""
+                                  type="checkbox"
+                                  checked={edit !== false && el.id === edit.id}
+                                  onChange={async () => {
+                                      setEdit(el);
+                                      p.setShowAdd();
+                                  }}
+                              />
+                          </td>
+                          <td>
+                              <LabeledSelect
+                                  label=""
+                                  value={`${el.ordernumber}`}
+                                  onClickCallback={(e) => {
+                                      console.log("LIMIT", e);
+                                      updateOrder(parseInt(e), el.id);
+                                  }}
+                                  id="newDay_ID"
+                              >
+                                  {mapResultLimit}
+                              </LabeledSelect>
+                          </td>
+                          {/* <td onClick={clickDetail}>{el.name}</td> */}
+                          <td onClick={clickDetail}>{el.question}</td>
+                          {/* <td>{el.filename}</td> */}
+                          <td className="text-center">
+                              <input
+                                  className=""
+                                  type="checkbox"
+                                  checked={false}
+                                  onChange={async (e) => {
+                                      e.preventDefault();
+                                      // console.log('click', el.id)
+                                      fetch(`/api/faq/deleteFAQ?id=${el.id}`)
+                                          .then((response) => response.json())
+                                          .then((data) => {
+                                              p.getFAQ();
+                                          });
+                                  }}
+                              />
+                          </td>
+                      </tr>
+                      {/* <tr key={`${el.id}-FAQ-delete`}>
                     <td className={`${display}`} colSpan={6}>
                         <p>{el.answer}</p>
                     </td>
                 </tr> */}
-            </React.Fragment>
-        );
-    });
+                  </React.Fragment>
+              );
+          })
+        : [];
 
     const FAQContainer = (
         <OutlinedSurface label="Review Team">
@@ -133,11 +137,7 @@ function MapFAQ(p: props) {
     return (
         <div>
             {FAQContainer}
-            {edit !== false ? (
-                <EditFAQ faq={edit} setEdit={setEdit} setFAQ={p.setFAQ} getFAQ={p.getFAQ} setShowAdd={p.setShowAdd} />
-            ) : (
-                <></>
-            )}
+            {edit !== false ? <EditFAQ faq={edit} setEdit={setEdit} setFAQ={p.setFAQ} getFAQ={p.getFAQ} setShowAdd={p.setShowAdd} /> : <></>}
         </div>
     );
 }
