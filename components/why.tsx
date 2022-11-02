6e13c94f6750f308cac564b1152ce91b39e2b558
Glenn Streetman <glennstreetman@gmail.com>
2022-11-02 23:09:17 +0000

    adjust spacing on title

--- components/why.tsx
+++ components/why.tsx
@@ -1,11 +1,10 @@
 import ParseMarkdown from "./../lib/parseMarkdown";
-
 import {siteText} from "../strapiAPI/getSiteText"
 
 
 //flex grid elements
 const gutter = " hidden lg:block lg:col-span-1 "; //2x
-const dataLeft = "p-2  col-span-12 md:col-span-12 lg:col-span-10"; //2x
+const dataLeft = "col-span-12 md:col-span-12 lg:col-span-10"; //2x
 
 interface props {
     siteText: siteText
@@ -14,8 +13,8 @@ interface props {
 function Why(p: props) {
 
     return (
-        <div className="flex flex-col bg-primary">
-            <div className="grid justify-items-center w-screen p-3">
+        <div className="flex flex-col bg-primary pt-3">
+            <div className="grid justify-items-center w-screen">
                 {p?.siteText?.aboutHeading && p.siteText.aboutHeading !== '' ? <div className="text-3xl font-bold text-center">
                     <ParseMarkdown text={p.siteText.aboutHeading} />
                 </div> : <></>}
@@ -23,7 +22,7 @@ function Why(p: props) {
             <div className="grid grid-cols-12">
                 <div className={gutter} />
                 <div className={dataLeft}>
-                    <div className="p-2">
+                    <div >
                         {p?.siteText?.aboutBody ? <ParseMarkdown text={p.siteText.aboutBody} /> : <></>}
                     </div>
                     <br />
