c9eea6cdecad40072bf246b533561a03d887a835
Glenn Streetman <glennstreetman@gmail.com>
2022-11-21 22:08:10 +0000

    check secret & revalidate for service slugs

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,31 +1,34 @@
 const staticRoutes = ["/", "/about",  "/calendar", "/careers",  "/contact", "/resumeSubmitted", "/service", "/thankyou" ];
 
-const revalidateLookup = {
-    faq: staticRoutes, 
-    holiday: ["/calendar"], 
-    holliday: ["/calendar"], 
-    service: ["/service"], 
-    team: ['/about'], 
-    bannertext: staticRoutes, 
-    contact: staticRoutes, 
-    googleMap: [ "/about", "/contact"], 
-    homepageintro: ['/'], 
-    ourstory: ['/', '/about'], 
-    pagetitle: ['/about'], 
-    servicehome: ["/service"], 
-    bannerimage: staticRoutes, 
-    sitelink: staticRoutes, 
-    sitetext: staticRoutes,
-}
-
-
-
 export default async function handler(req, res) {
+
+    const revalidateLookup = {
+        faq: staticRoutes, 
+        holiday: ["/calendar"], 
+        holliday: ["/calendar"], 
+        service: function(){
+            console.log('revalidating Now', req.query.name)
+            return [`/service/${req.query.name.replaceAll(' ', '_')}`]
+        }(), 
+        team: ['/about'], 
+        bannertext: staticRoutes, 
+        contact: staticRoutes, 
+        googleMap: [ "/about", "/contact"], 
+        homepageintro: ['/'], 
+        ourstory: ['/', '/about'], 
+        pagetitle: ['/about'], 
+        servicehome: ["/service"], 
+        bannerimage: staticRoutes, 
+        sitelink: staticRoutes, 
+        sitetext: staticRoutes,
+    }
+
     // Check for secret to confirm this is a valid request
     if (req.query.secret !== process.env.NEXT_REVALIDATE) {
         return res.status(401).json({ message: "Invalid token" });
     } else {
         try {
+
             const revalidateList = revalidateLookup?.[req?.query?.update] || []
             console.log('revalidating:', revalidateList)
             for(const el of revalidateList){
--- pages/api/revalidateHook.ts
+++ pages/api/revalidateHook.ts
@@ -1,26 +1,34 @@
 
 interface req {
     body: reqPayload
+    headers: any
 }
 
 interface reqPayload { 
     event: string, //entry.update
     createdAt: string,
     model: string, //faq, holiday, service, team, banner-text, contact, google-map, homepage-intro, our-story, page-title, service-home, banner-image, service-home, site-link, site-text
-    entry: {},
+    entry: {[key:string]: any}, //based upon model
+    headers: any
 }
 
 const test = async (req:req, res) => {
+        try {
+            if (req.headers.secret === process.env.NEXT_REVALIDATE) {
+                const update = req?.body?.model ? req.body.model.replaceAll('-', '') : []
+                const serviceName = update === 'service' ? req.body.entry.name : false
+                console.log('revalidate request received: ', update)
+                fetch(`${process.env.NEXTAUTH_URL}/api/revalidate?secret=${process.env.NEXT_REVALIDATE}&update=${update}&name=${serviceName}`)
+                res.status(200).json({update: update})
+            } else {
+                console.log('revalidateHook: Missing Secret')
+                res.status(400).json({ msg: "Error" });
+            }
+        } catch (err) {
+            console.log("/api/revalidateHook Error:", req.body ,err);
+            res.status(400).json({ msg: "Error" });
+        }
 
-    try {
-        const update = req?.body?.model ? req.body.model.replaceAll('-', '') : []
-        console.log('revalidate request received: ', update)
-        fetch(`${process.env.NEXTAUTH_URL}/api/revalidate?secret=${process.env.NEXT_REVALIDATE}&update=${update}`)
-        res.status(200).json({update: update})
-    } catch (err) {
-        console.log("/api/revalidateHook Error test Error:", req.body ,err);
-        res.status(400).json({ msg: "Error" });
-    }
 };
 
 export default test;
